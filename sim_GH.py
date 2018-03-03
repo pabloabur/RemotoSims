8caf4106ed907af613b8be6cf898d5dd89a719b6
Pablo Alejandro <pabloabur@gmail.com>
2018-03-02 22:48:44 -0300

    minor improvements on GH simulation

--- sim_GH.py
+++ sim_GH.py
@@ -7,7 +7,7 @@ from InterneuronPool import InterneuronPool
 from NeuralTract import NeuralTract
 from SynapsesFactory import SynapsesFactory
 
-def simulator():
+def simulator1():
 
     conf = Configuration('confGH.rmto')
 
@@ -35,6 +35,18 @@ def simulator():
     pools[1].listSpikes()
     pools[2].listSpikes()
 
+    # Calculate feedback gain
+    ## Calculate number of spikes
+    numRecruitedUnits = int(max(pools[0].poolSomaSpikes[:, 1]))
+    numberSpikes = [None] * numRecruitedUnits
+    firingRate = [None] * numRecruitedUnits
+    for i in xrange(numRecruitedUnits):
+        # Count number of spikes for each recruited unit
+        numberSpikes[i] = pools[0].poolSomaSpikes[:,1].tolist().count(i)
+        firingRate[i] = numberSpikes[i]/conf.simDuration_ms
+    ## Calcultate average MN activity
+    activity = np.mean(firingRate) * numRecruitedUnits
+
     plt.figure()
     plt.plot(pools[0].poolSomaSpikes[:, 0],
         pools[0].poolSomaSpikes[:, 1]+1, '.')
@@ -42,9 +54,55 @@ def simulator():
     plt.figure()
     plt.plot(t, MN_mV)
 
+    print '{} recruited units, with average activity of {}'.format(numRecruitedUnits,activity)
+
+def simulator2():
+
+    conf = Configuration('confGH.rmto')
+
+    pools = dict()
+    pools[0] = MotorUnitPool(conf, 'SOL')
+    pools[1] = NeuralTract(conf, 'CMExt')
+
+    Syn = SynapsesFactory(conf, pools)
+
+    t = np.arange(0.0, conf.simDuration_ms, conf.timeStep_ms)
+
+    MN_mV = np.zeros_like(t)
+
+    for i in xrange(0, len(t)):
+        pools[1].atualizePool(t[i]) # Neural tract
+        pools[0].atualizeMotorUnitPool(t[i]) # MN pool
+        MN_mV[i] = pools[0].unit[0].v_mV[1] 
+
+    pools[0].listSpikes()
+    pools[1].listSpikes()
+
+    # Calculate feedback gain
+    ## Calculate number of spikes
+    numRecruitedUnits = int(max(pools[0].poolSomaSpikes[:, 1]))
+    numberSpikes = [None] * numRecruitedUnits
+    firingRate = [None] * numRecruitedUnits
+    for i in xrange(numRecruitedUnits):
+        # Count number of spikes for each recruited unit
+        numberSpikes[i] = pools[0].poolSomaSpikes[:,1].tolist().count(i)
+        firingRate[i] = numberSpikes[i]/conf.simDuration_ms
+    ## Calcultate average MN activity
+    activity = np.mean(firingRate) * numRecruitedUnits
+
+    plt.figure()
+    plt.plot(pools[0].poolSomaSpikes[:, 0],
+        pools[0].poolSomaSpikes[:, 1]+1, '.')
+
+    plt.figure()
+    plt.plot(t, MN_mV)
+
+    print '{} recruited units, with average activity of {}'.format(numRecruitedUnits,activity)
+
 if __name__ == '__main__':
 
     np.__config__.show()
-    simulator()
+    simulator1()
+    simulator2()
     plt.show()
 
